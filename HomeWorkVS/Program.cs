f98b1f2a33014e03a58eebd76e2b75ba3f9be4dd
Vladyslav <vladchershev@gmail.com>
2025-06-07 22:07:06 +0200

    Revert "will revert it"
    This reverts commit 0296832487b52575775e68afcb1a830272ec4f6c.

--- HomeWorkVS/Program.cs
+++ HomeWorkVS/Program.cs
@@ -5,8 +5,6 @@ Console.Write("Enter your name: ");
 
 string name = Console.ReadLine();
 
-Console.WriteLine($"Hello, {name}! Welcome to the .NET 9.0 application.");
-
 Console.WriteLine($"Hello, {name}!");
 
 
